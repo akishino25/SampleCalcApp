b5254aa82df88c9f09704417a65289733031fe7d
akishino25 <akishino25@gmail.com>
2017-12-30 02:03:38 +0900

    CalcSetの単体試験途中

--- app/src/test/java/com/example/work/simplecalcapp/CalcSetTest.java
+++ app/src/test/java/com/example/work/simplecalcapp/CalcSetTest.java
@@ -11,17 +11,13 @@ import static org.junit.Assert.*;
  * Created by work on 2017/12/30.
  */
 public class CalcSetTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
 
     /**
      * enableClacCheckのテスト
      * 入力値を2つ、入力演算子を1つ設定して、計算可能チェックがtrueになることを確認
      */
     @Test
-    public void testEnableCalcCheck(){
+    public void testEnableCalcCheck() {
         CalcSet calcSet = new CalcSet();
         ArrayList<Integer> inputNums = new ArrayList<Integer>();
         inputNums.add(1);
@@ -39,7 +35,7 @@ public class CalcSetTest {
      * inputNumsが1つしか設定されていないため計算不可となることを確認
      */
     @Test
-    public void testEnableCalcCheck_ng_inputNums(){
+    public void testEnableCalcCheck_ng_inputNums() {
         CalcSet calcSet = new CalcSet();
         ArrayList<Integer> inputNums = new ArrayList<Integer>();
         inputNums.add(1);
@@ -56,7 +52,7 @@ public class CalcSetTest {
      * inputSymsが0つしか設定されていないため計算不可となることを確認
      */
     @Test
-    public void testEnableCalcCheck_ng_inputSyms(){
+    public void testEnableCalcCheck_ng_inputSyms() {
         CalcSet calcSet = new CalcSet();
         ArrayList<Integer> inputNums = new ArrayList<Integer>();
         inputNums.add(1);
@@ -66,6 +62,46 @@ public class CalcSetTest {
         calcSet.setInputSyms(inputSyms);
 
         assertFalse(calcSet.enableCalcCheck());
+    }
+
+    @Test
+    public void testConvertFormInputNumsToString() {
+        CalcSet calcSet = new CalcSet();
+        ArrayList<Integer> inputNums = new ArrayList<Integer>();
+        inputNums.add(1);
+        inputNums.add(2);
+        calcSet.setInputNums(inputNums);
+        assertEquals(calcSet.convertFromInputNumsToString(), "1,2");
+    }
+
+    @Test
+    public void testConvertFormInputSymsToString() {
+        CalcSet calcSet = new CalcSet();
+        ArrayList<String> inputSyms = new ArrayList<String>();
+        inputSyms.add("＋");
+        inputSyms.add("－");
+        calcSet.setInputSyms(inputSyms);
+        assertEquals(calcSet.convertFromInputSymsToString(), "＋,－");
+    }
+
+    @Test
+    public void testConvertFromStringToInputNums(){
+        CalcSet calcSet = new CalcSet();
+        ArrayList<Integer> expect = new ArrayList<Integer>();
+        expect.add(1);
+        expect.add(2);
+        ArrayList<Integer> actual = calcSet.convertFromStringToInputNums("1,2");
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    public void testConvertFromStringToInputSyms(){
+        CalcSet calcSet = new CalcSet();
+        ArrayList<String> expect = new ArrayList<>();
+        expect.add("＋");
+        expect.add("－");
+        ArrayList<String> actual = calcSet.convertFromStringToInputSyms("＋,－");
+        assertEquals(expect, actual);
     }
 
 }
